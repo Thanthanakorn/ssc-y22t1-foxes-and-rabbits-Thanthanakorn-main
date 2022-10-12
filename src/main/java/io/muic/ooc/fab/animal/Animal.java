1bb9209637b30f97fc26649b11a913a9686428a6
Thanakorn Hoonsil <thanankorn.hon@student.mahidol.edu>
2022-10-12 20:33:27 +0700

    Initial commit

---
+++ src/main/java/io/muic/ooc/fab/animal/Animal.java
@@ -0,0 +1,14 @@
+package io.muic.ooc.fab.animal;
+
+public abstract class Animal {
+
+    private boolean alive;
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+}
